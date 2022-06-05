5e6f2e4f4eab5343debaaf16b4c50e69789ca7c2
chekirko <victorche59@gmail.com>
2022-06-06 02:20:01 +0300

    додає рішення 9 задачі

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const btn = document.querySelector('.change-color');
+const bodyEl = document.querySelector('body');
+const spanEl = document.querySelector('.color');
+
+btn.addEventListener('click', onButtonClick);
+
+function onButtonClick(event) {
+  const newColor = getRandomHexColor();
+  bodyEl.style.backgroundColor = newColor;
+  spanEl.textContent = `${newColor}`;
+}
