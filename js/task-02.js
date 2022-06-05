120e5cd4282add3f8ba24166501c18a274e0acc3
chekirko <victorche59@gmail.com>
2022-06-05 13:12:24 +0300

    додає завдання 2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,16 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments'];
+
+const createIngredientsList = elements => {
+  const list = document.querySelector('#ingredients');
+
+  const items = elements.map(element => {
+    const liEl = document.createElement('li');
+    liEl.textContent = element;
+    liEl.classList.add('item');
+    return liEl;
+  });
+
+  return list.append(...items);
+};
+
+createIngredientsList(ingredients);
