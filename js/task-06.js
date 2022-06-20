939c57d7fd6e87bf29f815b8f43a19420630a0aa
chekirko <victorche59@gmail.com>
2022-06-20 10:45:47 +0300

    додає виправлення завдання 6

--- js/task-06.js
+++ js/task-06.js
@@ -3,7 +3,7 @@ const inputEl = document.querySelector('#validation-input');
 inputEl.addEventListener('blur', onInputBlur);
 
 function onInputBlur(event) {
-  event.currentTarget.value.length <= inputEl.dataset.length
+  event.currentTarget.value.length === Number(inputEl.dataset.length)
     ? inputEl.classList.add('valid')
     : inputEl.classList.add('invalid');
 }
