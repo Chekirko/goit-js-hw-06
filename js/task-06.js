45fd312f3f09fb22524babbc24f1ea73ba0e29c9
chekirko <victorche59@gmail.com>
2022-06-26 21:33:18 +0300

    додає остаточне рішення завдання 6

--- js/task-06.js
+++ js/task-06.js
@@ -3,7 +3,15 @@ const inputEl = document.querySelector('#validation-input');
 inputEl.addEventListener('blur', onInputBlur);
 
 function onInputBlur(event) {
-  event.currentTarget.value.length === Number(inputEl.dataset.length)
-    ? inputEl.classList.add('valid')
-    : inputEl.classList.add('invalid');
+  if (event.currentTarget.value.length === Number(inputEl.dataset.length)) {
+    if (inputEl.classList.contains('invalid')) {
+      inputEl.classList.remove('invalid');
+    }
+    inputEl.classList.add('valid');
+  } else {
+    if (inputEl.classList.contains('valid')) {
+      inputEl.classList.remove('valid');
+    }
+    inputEl.classList.add('invalid');
+  }
 }
